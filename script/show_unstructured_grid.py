87e66c648c7d64a9da4b70b1ee2df75e337eaf68
Carl <49546884+zhy2634153@users.noreply.github.com>
2020-05-17 10:37:26 +0800

    Create show_unstructured_grid.py

---
+++ script/show_unstructured_grid.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#-*-coding:utf-8-*-
+
+import vtk
+
+filename = "Job-1-C3D20.vtk"
+# filename = "test2.vtk"
+# filename = "Hexahedron.vtk"
+
+colors = vtk.vtkNamedColors()
+
+reader = vtk.vtkDataSetReader()
+reader.SetFileName(filename)
+
+reader.Update()
+
+mapper = vtk.vtkDataSetMapper()
+mapper.SetInputConnection(reader.GetOutputPort())
+
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetColor(colors.GetColor3d("Green"))
+
+ren = vtk.vtkRenderer()
+ren.AddActor(actor)
+ren.SetBackground(colors.GetColor3d("White"))
+
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+renWin.SetSize(600, 600)
+
+iren  = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+iren.Initialize()
+iren.Start()
